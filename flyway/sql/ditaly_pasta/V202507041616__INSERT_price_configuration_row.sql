93ce889526c16fb7633f4fbc9f5a395c8fa0535e
FMDEV <fra.sotomen@gmail.com>
2025-10-14 21:14:00 -0300

    update, prod .env

--- flyway/sql/ditaly_pasta/V202507041616__INSERT_price_configuration_row.sql
+++ flyway/sql/ditaly_pasta/V202507041616__INSERT_price_configuration_row.sql
@@ -1,7 +1,7 @@
 -- Insertar registro inicial en ditaly_pasta.price_configuration
 INSERT INTO ditaly_pasta.price_configuration
-(code, price_configuration, franchise_configuration, variable_formula, is_deleted, is_confirmed, created_at, updated_at, confirmed_at, deleted_at, created_by, confirmed_by, updated_by, deleted_by)
-VALUES('cd746343-baf4-4359-b2e6-9bd829631e30', 'PRODUCT_NORMAL_IVA', 'e8a80b9f-38d5-4c3d-8add-bb28a2493e26', 'ee4eabde-242b-4252-9a81-8266ad9b9336', NULL, true, '2025-07-17 11:29:02.059', NULL, NULL, NULL, '1b4ec1e3-2b23-461f-83f7-3901f77c5ddc', '1b4ec1e3-2b23-461f-83f7-3901f77c5ddc', NULL, NULL); 
+(code, price_configuration, franchise_configuration, variable_formula, price_type, is_deleted, is_confirmed, created_at, updated_at, confirmed_at, deleted_at, created_by, confirmed_by, updated_by, deleted_by)
+VALUES('cd746343-baf4-4359-b2e6-9bd829631e30', 'PRODUCT_NORMAL_IVA', 'e8a80b9f-38d5-4c3d-8add-bb28a2493e26', 'ee4eabde-242b-4252-9a81-8266ad9b9336', 1, NULL, true, '2025-07-17 11:29:02.059', NULL, NULL, NULL, '1b4ec1e3-2b23-461f-83f7-3901f77c5ddc', '1b4ec1e3-2b23-461f-83f7-3901f77c5ddc', NULL, NULL); 
 
 
 INSERT INTO ditaly_pasta.fiscal_configuration_detail
--- flyway/sql/ditaly_pasta/V202507041621__INSERT_additional_price_configuration.sql
+++ flyway/sql/ditaly_pasta/V202507041621__INSERT_additional_price_configuration.sql
@@ -1,8 +1,8 @@
 -- INSERT additional price configuration data
 INSERT INTO ditaly_pasta.price_configuration
-(id, code, price_configuration, franchise_configuration, variable_formula, is_deleted, is_confirmed, created_at, updated_at, confirmed_at, deleted_at, created_by, confirmed_by, updated_by, deleted_by)
+(id, code, price_configuration, franchise_configuration, variable_formula, price_type, is_deleted, is_confirmed, created_at, updated_at, confirmed_at, deleted_at, created_by, confirmed_by, updated_by, deleted_by)
 OVERRIDING SYSTEM VALUE
-VALUES(2, 'ffd4f457-5fff-42c5-a36d-07be4b6ad39a', 'CATALOG_NORMAL_IVA', 'e8a80b9f-38d5-4c3d-8add-bb28a2493e26', 'ee4eabde-242b-4252-9a81-8266ad9b9336', NULL, true, '2025-07-31 18:14:44.164', NULL, NULL, NULL, '1b4ec1e3-2b23-461f-83f7-3901f77c5ddc', '1b4ec1e3-2b23-461f-83f7-3901f77c5ddc', NULL, NULL);
+VALUES(2, 'ffd4f457-5fff-42c5-a36d-07be4b6ad39a', 'CATALOG_NORMAL_IVA', 'e8a80b9f-38d5-4c3d-8add-bb28a2493e26', 'ee4eabde-242b-4252-9a81-8266ad9b9336', 4, NULL, true, '2025-07-31 18:14:44.164', NULL, NULL, NULL, '1b4ec1e3-2b23-461f-83f7-3901f77c5ddc', '1b4ec1e3-2b23-461f-83f7-3901f77c5ddc', NULL, NULL);
 
 -- INSERT additional fiscal configuration detail data
 INSERT INTO ditaly_pasta.fiscal_configuration_detail
